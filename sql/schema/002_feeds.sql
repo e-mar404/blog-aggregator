48713c014c4c19befdc069c25757e47ec82d68c0
e-mar404 <egmarinn@gmail.com>
2025-04-20 18:18:29 -0500

    add feed_follows table

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -8,5 +8,15 @@ CREATE TABLE feeds (
   updated_at TIMESTAMP NOT NULL
 );
 
+CREATE TABLE feed_follows (
+  id UUID PRIMARY KEY,
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  feed_id UUID REFERENCES feeds(id),
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  CONSTRAINT unique_follow UNIQUE (user_id, feed_id)
+);
+
 -- +goose Down
 DROP TABLE feeds;
+DROP TABLE feed_follows;
